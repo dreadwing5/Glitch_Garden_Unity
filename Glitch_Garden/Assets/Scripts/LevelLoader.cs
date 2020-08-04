81055e2ecccd29ddf5549be8f260903c8409e615
dreadwing5 <sachinspyk@outlook.com>
2020-08-05 00:46:13 +0530

    Alpha Build

---
+++ Glitch_Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float startScreenLoadTime = 4f;
+    int currentSceneIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            LoadStartScene();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LoadStartScene()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(startScreenLoadTime);
+        SceneManager.LoadScene("Start Scene");
+    }
+
+}
