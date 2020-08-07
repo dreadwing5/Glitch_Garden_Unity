c68b72f2f3a518b21b4b9dd2c8e6ad0f472c5967
dreadwing5 <sachinspyk@outlook.com>
2020-08-08 02:40:27 +0530

    Defenders Animation

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f,5f)]
+    [SerializeField] float currentSpeed = 1f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left *Time.deltaTime * currentSpeed); 
+        /* The  Attacker starts moving form right to left on each frame, time.deltaTime is
+        used to make the attacker speed framerate independent */
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] GameObject attackerPrefab;
+    [SerializeField] float maxSpawnDelay = 0f;
+    [SerializeField] float minSpawnDelay = 5f;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay)); /* Wait Time To Spawn Attackers */
+            SpawnAttacker();
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab,transform.position,Quaternion.identity);
+        /* Instianting Attacker at a random wayPt */
+    }
+}
