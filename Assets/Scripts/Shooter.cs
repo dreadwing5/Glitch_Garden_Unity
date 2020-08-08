f50879d4c7716097c57add5fd846d51b227565bf
dreadwing5 <sachinspyk@outlook.com>
2020-08-08 18:38:14 +0530

    Shooter

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab, Gun;
+    [SerializeField] float moveSpeed =1f;
+    public void Fire()
+    {
+        Instantiate(projectilePrefab,Gun.transform.position,transform.rotation);
+    }
+}
