41045870f6da5ae18f2c1318e5fec9a55263fc5d
dreadwing5 <sachinspyk@outlook.com>
2020-08-16 21:06:44 +0530

    New

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,25 +7,33 @@ public class Health : MonoBehaviour
     [SerializeField] float currentHealth = 500f;
     [SerializeField] GameObject deathVFX;
     [SerializeField] AudioClip deathSFX;
-    [SerializeField] [Range(0f,1f)] float deathSFXVolume;
+    [SerializeField] [Range(0f, 1f)] float deathSFXVolume;
 
-    public void DealDamage(float damage){
-        currentHealth-=damage;
-        if(currentHealth<=0)
+    public void DealDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
         {
             Die();
         }
     }
 
-    public float SetHealth(){
+    public float SetHealth()
+    {
         return currentHealth;
     }
 
-    public void Die(){
-        Debug.Log("Attacker is Dead!");
-        AudioSource.PlayClipAtPoint(deathSFX,Camera.main.transform.position,deathSFXVolume);
-        GameObject DeathVFX  = Instantiate(deathVFX,transform.position,Quaternion.identity) as GameObject;
+    public void Die()
+    {
+        TriggerDeathVFX();
         Destroy(gameObject);
     }
-   
+
+    private void TriggerDeathVFX()
+    {
+        AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSFXVolume);
+        GameObject DeathVFX = Instantiate(deathVFX, transform.position, Quaternion.identity) as GameObject;
+        Destroy(DeathVFX,1f);
+    }
+
 }
